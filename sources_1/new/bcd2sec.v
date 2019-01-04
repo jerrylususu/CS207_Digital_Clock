a2a5044cc9bbf0db93a34ee5609320ba3758f621
Jerry Lu <jerrylu.mail@qq.com>
2019-01-04 15:16:43 +0800

    add mod for time input

--- sources_1/new/bcd2sec.v
+++ sources_1/new/bcd2sec.v
@@ -7,6 +7,6 @@ module bcd2sec(seconds,h1,h2,m1,m2,s1,s2);
     input [3:0] h1,h2,m1,m2,s1,s2;
     output [16:0] seconds;
     
-    assign seconds=(h1*10+h2)*3600+(m1*10+m2)*60+s1*10+s2;
+    assign seconds=((h1*10+h2)*3600+(m1*10+m2)*60+s1*10+s2)%86400;
 
 endmodule
